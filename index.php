52293d044cc02a544146340b791d8c9e9d69264e
GiovanniCalia <johnca@outlook.it>
2022-05-02 17:46:44 +0200

    Aggiornamento disponibilità antiparassitario

--- index.php
+++ index.php
@@ -5,10 +5,34 @@ class prodottiECommerce{
     private $cuccia;
     private $lettiera;
     private $antiparassitario;
+    
+
+    public function disponibilità(string $_disponibilità){
+        if ($_disponibilità === "maggio" || $_disponibilità === "giugno" || $_disponibilità === "luglio" || $_disponibilità === "agosto"){
+            return "L'antiparassitario è disponibile";
+        } else{
+            return "L'antiparassitario non è disponibile";
+        }
+    }
+
+    public function getAntiparassitario()
+    {
+        return $this->antiparassitario;
+    }
+
+
+    public function setAntiparassitario($antiparassitario)
+    {
+        $this->antiparassitario = $antiparassitario;
+        
+        return $this;
+    }
 }
 
 
 $eCommerce = new prodottiECommerce();
-var_dump($eCommerce)
+var_dump($eCommerce);
+echo $eCommerce->disponibilità("luglio");
+
 ?>
  
